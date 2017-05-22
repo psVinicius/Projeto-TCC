fd4e0fd5043d7dc0ab537fa58d29b3b465341249
Vinicius Alves <psviniciusalves@gmail.com>
2017-05-22 16:54:05 -0300

    Neste commit o banco ganhou um backup novo e o projeto co teve atualizações na modelo tabela produto com as novas linhas de link e marca

---
+++ ConsumidorOnline V.ModelosTabelas.NovasColunas.Produtos/src/ModeloTabela/Produto.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ModeloTabela;
+
+/**
+ *
+ * @author Vinicius Alves
+ */
+public class Produto {
+    private int id;
+    private String nome;
+    private double preco;
+    private String link;
+    private String marca;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the preco
+     */
+    public double getPreco() {
+        return preco;
+    }
+
+    /**
+     * @param preco the preco to set
+     */
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    /**
+     * @return the link
+     */
+    public String getLink() {
+        return link;
+    }
+
+    /**
+     * @param link the link to set
+     */
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    /**
+     * @return the marca
+     */
+    public String getMarca() {
+        return marca;
+    }
+
+    /**
+     * @param marca the marca to set
+     */
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+}
