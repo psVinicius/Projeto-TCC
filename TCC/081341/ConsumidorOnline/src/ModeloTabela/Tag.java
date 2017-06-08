06583ab20bb02d98d8ab5da7abdcc2449b4e5546
Vinicius Alves <psviniciusalves@gmail.com>
2017-06-08 13:43:08 -0300

    Neste commit, mais uma versão do CO foi adicionada, erros foram tratados, TCC foi atualizado com o novo pacote de formularios do CO imagens novas também

---
+++ TCC/081341/ConsumidorOnline/src/ModeloTabela/Tag.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ModeloTabela;
+
+/**
+ *
+ * @author Vinicius Alves
+ */
+public class Tag {
+    
+    private int id;
+    private String nome_tag;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome_tag() {
+        return nome_tag;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome_tag(String nome) {
+        this.nome_tag = nome;
+    }
+    
+}
